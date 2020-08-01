f6a6d68bbfc5800f42c122602bcf723fb4d1127a
Yassine1231 <mbouh050@uottawa.ca>
2020-08-01 13:59:01 -0400

    final version

--- APP.py
+++ APP.py
@@ -1,5 +1,3 @@
-import os
-import flask
 import dash
 import dash_daq as daq
 import dash_core_components as dcc
@@ -56,7 +54,7 @@ tab_style = {
     'border': '1px solid #111111',
     'background':'#111111',
     'color':'#7FDBFF',
-    'padding': '6px'
+    'padding': '3px'
 }
 tab_selected_style = {
     'borderBottom': '2px solid #7FDBFF',
@@ -65,14 +63,14 @@ tab_selected_style = {
     'borderTop': '1px solid #111111',
     'backgroundColor': '#272a2e',
     'color': '#7FDBFF',
-    'padding': '6px',
+    'padding': '3px',
     'fontWeight':'bold'
 }
 main_tab_style = {
     'border': '1px solid #111111',
     'background':'#111111',
     'color':'#f5f6f7',
-    'padding': '6px',
+    'padding': '3px',
 }
 main_tab_selected = {
     'borderBottom': '2px solid white',
@@ -81,34 +79,36 @@ main_tab_selected = {
     'borderTop': '1px solid #111111',
     'backgroundColor': '#272a2e',
     'color': '#f5f6f7',
-    'padding': '6px',
+    'padding': '3px',
     'fontWeight':'bold'
 }
 
-server = flask.Flask(__name__)
           
 #initialisation de la page dash
-app = dash.Dash(__name__, server = server, external_stylesheets=['assets/style.css'])
+app = dash.Dash(__name__, 
+             meta_tags=[{"name":"description","content":"my UI app"},{"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 app.title = "UI pour IRESEN"
+server = app.server 
+
 
 #visualisation de la page
 app.layout =html.Div(
-    [
+    [	
         html.Header(className="flex-display row", children=[
-            html.Img(src=app.get_asset_url('domaines.png'), className='icon'),
+            html.Img(src=app.get_asset_url('domaines.webp'), className='icon', alt='icone des domaines'),
             html.Div(className='seven columns', children=[
                 html.H1('Interface graphique pour serre intelligente', style={'textAlign': 'center', 'margin-top':'30px'}),
                 html.Br(),
                 html.H6('Visualisation des paramètres avec un serveur local', style={'textAlign': 'center', 'padding': 5}),
             ]),
-            html.Img(src=app.get_asset_url("iresen.png"), className='icon_iresen'),
+            html.Img(src=app.get_asset_url("Iresen.webp"), className='icon_iresen', alt='icone IRESEN'),
         ]),
         html.Main([
             dcc.Tabs(value='tab_1', children=[
                 dcc.Tab(label='Vue générale', value='tab_1', style= main_tab_style, selected_style= main_tab_selected, children=[
                     html.Div(className='six columns', children=[
-                        html.H2(children='Image de la serre', className='six columns', style={'margin-left': 140, 'padding': 5}),
-                        html.Img(src=app.get_asset_url('serre.png'), className='serre'),
+                        html.H2(children='Image de la serre', className='twelve columns', style={'textAlign':'center'}),
+                        html.Img(src=app.get_asset_url('serre.webp'), className='serre', alt='image de la serre'),
                     ]),
                     html.Div(className='six columns', children=[
                         html.H2(children='Valeur des capteurs', className='twelve columns', style={'textAlign':'center'}),
@@ -124,8 +124,8 @@ app.layout =html.Div(
                 ]),
                 dcc.Tab(label='Humidité du sol', style=tab_style, selected_style=tab_selected_style, children=[
                     html.Div(className= 'three columns', children=[
-                        html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
-                        html.Img(src=app.get_asset_url('serre moist.png'), className='img_sensor'),
+                        html.H4(children='Emplacement des capteurs',className='twelve columns', style={'margin-left':20,'textAlign': 'center'}),
+                        html.Img(src=app.get_asset_url('serre moist.webp'), className='img_sensor', alt='image de la serre'),
                     ]),
                     html.Div(className='six columns', children=[
                         html.H4(children='Humidité du sol', style={'textAlign': 'center'}),
@@ -135,7 +135,7 @@ app.layout =html.Div(
                     ]),
                     html.Div(className='three columns', children=[
                         html.H4(children='Image du capteur'),
-                        html.Img(src=app.get_asset_url('moisture.png'), className='img_capt'),
+                        html.Img(src=app.get_asset_url('moisture.webp'), className='img_capt', alt='image du capteur'),
                     ]),
                     html.Div(className='twelve columns', children=[
                             render_graph('moisture','Humidité du sol')
@@ -144,7 +144,7 @@ app.layout =html.Div(
                 dcc.Tab(label='Température du sol', style=tab_style, selected_style=tab_selected_style, children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
-                        html.Img(src=app.get_asset_url('serre temp.png'), className='img_sensor'),
+                        html.Img(src=app.get_asset_url('serre temp.webp'), className='img_sensor', alt='image de la serre'),
                     ]),
                     html.Div(className='six columns', children=[
                         html.H4(children='Température du sol', style={'textAlign': 'center'}),
@@ -154,7 +154,7 @@ app.layout =html.Div(
                     ]),
                     html.Div(className='three columns', children=[
                         html.H4(children='Image du capteur'),
-                        html.Img(src=app.get_asset_url('temp.png'), className='img_capt'),
+                        html.Img(src=app.get_asset_url('temp.webp'), className='img_capt', alt='image du capteur'),
                     ]),
                     html.Div(className='twelve columns', children=[
                         render_graph('temperature','Température du sol')              
@@ -163,7 +163,7 @@ app.layout =html.Div(
                 dcc.Tab(label='Température ambiante', style=tab_style, selected_style=tab_selected_style, children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
-                        html.Img(src=app.get_asset_url('serre both.png'), className='img_sensor'),
+                        html.Img(src=app.get_asset_url('serre both.webp'), className='img_sensor', alt='image de la serre'),
                     ]),
                     html.Div(className='six columns', children=[
                         html.H4(children='Température ambiante', style={'textAlign': 'center'}),
@@ -173,7 +173,7 @@ app.layout =html.Div(
                     ]),
                     html.Div(className='three columns', children=[
                         html.H4(children='Image du capteur'),
-                        html.Img(src=app.get_asset_url('temp and humidity.png'), className='img_capt'),
+                        html.Img(src=app.get_asset_url('temp and humidity.webp'), className='img_capt', alt='image du capteur'),
                     ]),
                     html.Div(className='twelve columns', children=[
                         render_graph('temperature','Température ambiante')
@@ -182,7 +182,7 @@ app.layout =html.Div(
                 dcc.Tab(label='Ensoleillement', style=tab_style, selected_style=tab_selected_style, children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
-                        html.Img(src=app.get_asset_url('serre enso.png'), className='img_sensor'),
+                        html.Img(src=app.get_asset_url('serre enso.webp'), className='img_sensor', alt='image de la serre'),
                     ]),
                     html.Div(className='six columns', children=[
                         html.H4(children='Ensoleillement', style={'textAlign': 'center'}),
@@ -192,7 +192,7 @@ app.layout =html.Div(
                     ]),
                     html.Div(className='three columns', children=[
                         html.H4(children='Image du capteur'),
-                        html.Img(src=app.get_asset_url('ensoleillement.png'), className='img_capt'),
+                        html.Img(src=app.get_asset_url('ensoleillement.webp'), className='img_capt', alt='image du capteur'),
                     ]),
                     html.Div(className='twelve columns', children=[
                         render_graph('humidity','Ensoleillement')
@@ -201,7 +201,7 @@ app.layout =html.Div(
                 dcc.Tab(label='CO2', style=tab_style, selected_style=tab_selected_style, children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
-                        html.Img(src=app.get_asset_url('serre co.png'), className='img_sensor'),
+                        html.Img(src=app.get_asset_url('serre co.webp'), className='img_sensor', alt='image de la serre'),
                     ]),
                     html.Div(className='six columns', children=[
                         html.H4(children='CO2', style={'textAlign': 'center'}),
@@ -211,7 +211,7 @@ app.layout =html.Div(
                     ]),
                     html.Div(className='three columns', children=[
                         html.H4(children='Image du capteur'),
-                        html.Img(src=app.get_asset_url('CO2.png'), className='img_capt'),
+                        html.Img(src=app.get_asset_url('CO2.webp'), className='img_capt', alt='image du capteur'),
                     ]),
                     html.Div(className='twelve columns', children=[
                         render_graph('moisture','CO2')
@@ -220,7 +220,7 @@ app.layout =html.Div(
                 dcc.Tab(label='Humidité relative', style=tab_style, selected_style=tab_selected_style, children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
-                        html.Img(src=app.get_asset_url('serre both.png'), className='img_sensor'),
+                        html.Img(src=app.get_asset_url('serre both.webp'), className='img_sensor', alt='image de la serre'),
                     ]),
                     html.Div(className='six columns', children=[
                         html.H4(children='Humidité relative', style={'textAlign': 'center'}),
@@ -230,7 +230,7 @@ app.layout =html.Div(
                     ]),
                     html.Div(className='three columns', children=[
                         html.H4(children='Image du capteur'),
-                        html.Img(src=app.get_asset_url('temp and humidity.png'), className='img_capt'),
+                        html.Img(src=app.get_asset_url('temp and humidity.webp'), className='img_capt', alt='image du capteur'),
                     ]),
                     html.Div(className='twelve columns', children=[
                         render_graph('humidity','Humidité relative')
@@ -240,5 +240,6 @@ app.layout =html.Div(
     ])
 ])
 
+
 if __name__ == '__main__':
-    app.run_server(debug = True)
+    app.run_server(debug=False)
