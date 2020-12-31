34c7c3c358a9cfd8fb24bcd27d9305829be643cf
Yassine1231 <mbouh050@uottawa.ca>
2020-12-31 15:46:18 +0100

    Update modebar

--- APP.py
+++ APP.py
@@ -1,3 +1,5 @@
+import os
+import flask
 import dash
 import dash_daq as daq
 import dash_core_components as dcc
@@ -19,77 +21,47 @@ def render_gauge(name, parameter, unit):
       color = {"gradient":True,"ranges":{"green":[0,60],"yellow":[60,80],"red":[80,100]}},
       scale = {'start':0, 'interval': 5},
       )
-
+config = {'displayModeBar': False,'displaylogo': False, 'scrollZoom': True}
 def render_graph(yvalue, label):
-    return dcc.Graph(
-      config={'displaylogo': False},
-      figure ={ 'data': [{'x': df['year'], 'y': df[yvalue], 'type': 'bar'}],
-                'layout' : {
-                'title' : label + ' VS Temps',
-                'xaxis' : dict(
-                              title= 'Temps',
-                              titlefont=dict(
-                                            family = 'Arial',
-                                            size = 20,
-                                            color = '#7FDBFF'
-                                        )
-                          ),
-                'yaxis' : dict(
-                              title= label,
-                              titlefont=dict(
-                                            family = 'Arial',
-                                            size = 20,
-                                            color = '#7FDBFF'
-                                        )
-                          ),
-                'plot_bgcolor': '#1d242e',
-                'paper_bgcolor': '#1d242e',
-                'font': {'color': '#7FDBFF'}                                                       
-                }
-              }
-          )
+    return dcc.Graph(  
+        config= config,
+        figure =dict(
+            data=[
+                dict(
+                    x=df['year'], y=df[yvalue], type='bar',
+                )
+            ],
+            layout=dict(
+                title = label + ' VS Temps',
+                xaxis = dict(
+                    title= 'Temps',
+                    titlefont=dict(
+                        family = 'Arial',
+                        size = 20,
+                        color = '#7FDBFF'
+                        )
+                    ),
+                yaxis = dict(
+                    title= label,
+                    titlefont=dict(
+                        family = 'Arial',
+                        size = 20,
+                        color = '#7FDBFF'
+                        )
+                    ),
+                plot_bgcolor= '#1d242e',
+                paper_bgcolor= '#1d242e',
+                font= {'color': '#7FDBFF'},                                                     
+            )
+        ),
+        style={'overflowX':'scroll'},
+        id= "graph"+label,
+    )
 
-#styles des onglets utilisés
-tab_style = {
-    'border': '1px solid #111111',
-    'background':'#111111',
-    'color':'#7FDBFF',
-    'padding': '3px'
-}
-tab_selected_style = {
-    'borderBottom': '2px solid #7FDBFF',
-    'borderRight': '1px solid #111111',
-    'borderLeft' : '1px solid #111111',
-    'borderTop': '1px solid #111111',
-    'backgroundColor': '#272a2e',
-    'color': '#7FDBFF',
-    'padding': '3px',
-    'fontWeight':'bold'
-}
-main_tab_style = {
-    'border': '1px solid #111111',
-    'background':'#111111',
-    'color':'#f5f6f7',
-    'padding': '3px',
-}
-main_tab_selected = {
-    'borderBottom': '2px solid white',
-    'borderRight': '1px solid #111111',
-    'borderLeft' : '1px solid #111111',
-    'borderTop': '1px solid #111111',
-    'backgroundColor': '#272a2e',
-    'color': '#f5f6f7',
-    'padding': '3px',
-    'fontWeight':'bold'
-}
-
-          
 #initialisation de la page dash
-app = dash.Dash(__name__, 
+app = dash.Dash(__name__, title='UI pour IRESEN',
              meta_tags=[{"name":"description","content":"my UI app"},{"name": "viewport", "content": "width=device-width, initial-scale=1"}])
-app.title = "UI pour IRESEN"
 server = app.server 
-
 
 #visualisation de la page
 app.layout =html.Div(
@@ -104,8 +76,8 @@ app.layout =html.Div(
             html.Img(src=app.get_asset_url("Iresen.webp"), className='icon_iresen', alt='icone IRESEN'),
         ]),
         html.Main([
-            dcc.Tabs(value='tab_1', children=[
-                dcc.Tab(label='Vue générale', value='tab_1', style= main_tab_style, selected_style= main_tab_selected, children=[
+            dcc.Tabs(id='tabs', value='tab_1', className='tabs-container', parent_className='custom-tabs', children=[
+                dcc.Tab(label='Vue générale', value='tab_1', className='custom-tab', selected_className='custom-tab--selected', children=[
                     html.Div(className='six columns', children=[
                         html.H2(children='Image de la serre', className='twelve columns', style={'textAlign':'center'}),
                         html.Img(src=app.get_asset_url('serre.webp'), className='serre', alt='image de la serre'),
@@ -122,7 +94,7 @@ app.layout =html.Div(
                         ])
                     ]),
                 ]),
-                dcc.Tab(label='Humidité du sol', style=tab_style, selected_style=tab_selected_style, children=[
+                dcc.Tab(label='Humidité du sol', className='custom-tab', selected_className='custom-tab--selected', children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs',className='twelve columns', style={'margin-left':20,'textAlign': 'center'}),
                         html.Img(src=app.get_asset_url('serre moist.webp'), className='img_sensor', alt='image de la serre'),
@@ -141,7 +113,7 @@ app.layout =html.Div(
                             render_graph('moisture','Humidité du sol')
                     ])
                 ]),
-                dcc.Tab(label='Température du sol', style=tab_style, selected_style=tab_selected_style, children=[
+                dcc.Tab(label='Température du sol', className='custom-tab', selected_className='custom-tab--selected', children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
                         html.Img(src=app.get_asset_url('serre temp.webp'), className='img_sensor', alt='image de la serre'),
@@ -160,7 +132,7 @@ app.layout =html.Div(
                         render_graph('temperature','Température du sol')              
                     ])
                 ]),
-                dcc.Tab(label='Température ambiante', style=tab_style, selected_style=tab_selected_style, children=[
+                dcc.Tab(label='Température ambiante', className='custom-tab', selected_className='custom-tab--selected', children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
                         html.Img(src=app.get_asset_url('serre both.webp'), className='img_sensor', alt='image de la serre'),
@@ -179,7 +151,7 @@ app.layout =html.Div(
                         render_graph('temperature','Température ambiante')
                     ])
                 ]),
-                dcc.Tab(label='Ensoleillement', style=tab_style, selected_style=tab_selected_style, children=[
+                dcc.Tab(label='Ensoleillement', className='custom-tab', selected_className='custom-tab--selected', children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
                         html.Img(src=app.get_asset_url('serre enso.webp'), className='img_sensor', alt='image de la serre'),
@@ -198,7 +170,7 @@ app.layout =html.Div(
                         render_graph('humidity','Ensoleillement')
                     ])
                 ]),
-                dcc.Tab(label='CO2', style=tab_style, selected_style=tab_selected_style, children=[
+                dcc.Tab(label='CO2', className='custom-tab', selected_className='custom-tab--selected', children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
                         html.Img(src=app.get_asset_url('serre co.webp'), className='img_sensor', alt='image de la serre'),
@@ -217,7 +189,7 @@ app.layout =html.Div(
                         render_graph('moisture','CO2')
                     ])
                 ]),      
-                dcc.Tab(label='Humidité relative', style=tab_style, selected_style=tab_selected_style, children=[
+                dcc.Tab(label='Humidité relative', className='custom-tab', selected_className='custom-tab--selected', children=[
                     html.Div(className= 'three columns', children=[
                         html.H4(children='Emplacement des capteurs', style={'textAlign': 'center', 'margin-left':20}),
                         html.Img(src=app.get_asset_url('serre both.webp'), className='img_sensor', alt='image de la serre'),
@@ -235,11 +207,24 @@ app.layout =html.Div(
                     html.Div(className='twelve columns', children=[
                         render_graph('humidity','Humidité relative')
                     ])
-                ]),        
+                ]),      
         ])
     ])
 ])
 
 
+class MyFlask(flask.Flask):
+    def get_send_file_max_age(self, name):
+        if name.lower().endswith('.webp'):
+            return 60
+        if name.lower().endswith('.css?m=1596413279.0'):
+        	return 60
+        return flask.Flask.get_send_file_max_age(self, name)
+
+@server.route('/assets/<path:path>')
+def serve_static(path):
+	root_dir = os.getcwd()
+	return flask.send_from_directory(os.path.joint(root_dir, 'assets'),path)
+
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
